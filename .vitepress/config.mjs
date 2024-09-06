a8fb8a5ccd605827bcfc0d7ddd55cfb53e602afa
Vivien <34387011+vivien8261@users.noreply.github.com>
2024-09-06 18:12:45 +0800

    init

---
+++ .vitepress/config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+    title: "互联网典中典收录",
+    description: "",
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '典中典', link: '/jokes' }
+        ],
+        sidebar: [
+            {
+                text: '典中典',
+                items: [
+                    { text: '赛博文盲', link: '/jokes/cyber-illiteracy.md' },
+                ]
+            }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vivien8261/Jokes-on-the-Internet' }
+        ]
+    }
+})
---
+++ index.md
@@ -0,0 +1,14 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "互联网"
+  text: "典中典收录"
+  tagline: 典！
+  actions:
+    - theme: brand
+      text: 开始浏览！
+      link: /jokes
+---
+
