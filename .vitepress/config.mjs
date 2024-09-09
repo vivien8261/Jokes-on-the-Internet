97f7b450d3dc3a75c7f96c21e468290f7160c70d
Vivien <34387011+vivien8261@users.noreply.github.com>
2024-09-09 14:35:12 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    title: "互联网典中典收录",
-    description: "",
+    base: '/Jokes-on-the-Internet/',
+    title: '互联网典中典收录',
+    description: '',
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
